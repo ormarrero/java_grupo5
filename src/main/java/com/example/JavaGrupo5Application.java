bb641a311e969aeae3d1923d0135d4c6ca421848
ormarrero <104150268+ormarrero@users.noreply.github.com>
2023-02-05 19:55:40 +0100

    Initial commit

---
+++ src/main/java/com/example/JavaGrupo5Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGrupo5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGrupo5Application.class, args);
+	}
+
+}
