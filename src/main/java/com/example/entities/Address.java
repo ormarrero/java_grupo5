6d5c2fc3ac48759a5cf637d5a635c88f19eed7b6
JoseIgnacioGJ <joigarja@gmail.com>
2023-02-07 15:39:50 +0100

    Creamos la clase Address.java

---
+++ src/main/java/com/example/entities/Address.java
@@ -0,0 +1,23 @@
+package com.example.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter // Métodos getter
+@Setter // Métodos setter
+@NoArgsConstructor // Constructor sin parámetros
+@AllArgsConstructor // Constructor con todos los parámetros
+@ToString // Método toString()
+@Builder // Patrón de diseño builder, permite construir objetos dinámicamente concatenando métodos
+
+@Entity
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String calle;
+    private Integer numero;
+    private String ciudad;
+    private Integer cp;
+
+}
