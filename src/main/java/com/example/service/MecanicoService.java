347bd38ccc325cbf7f3b07b4da8fbbcd0dd60b0f
Robertron95 <robertron561@gmail.com>
2023-02-16 08:58:32 +0100

    Servicios Mecánico y Taller

---
+++ src/main/java/com/example/service/MecanicoService.java
@@ -0,0 +1,13 @@
+package com.example.service;
+
+import com.example.entities.Mecanico;
+
+import java.util.List;
+
+public interface MecanicoService {
+
+    // Operaciones Crud
+    List<Mecanico> findAll();
+
+    // Lógica de negocio
+}
---
+++ src/main/java/com/example/service/TallerServiceImpl.java
@@ -0,0 +1,21 @@
+package com.example.service;
+
+import com.example.entities.Taller;
+import com.example.repository.TallerRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class TallerServiceImpl implements TallerService {
+
+    private final TallerRepository tallerRepository;
+
+    @Override
+    public List<Taller> findAll() {
+
+        return tallerRepository.findAll();
+    }
+}
