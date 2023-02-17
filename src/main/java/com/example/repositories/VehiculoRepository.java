6fdf1872545b31792a44e3eee0b7c7c5bc99ccb7
ormarrero <104150268+ormarrero@users.noreply.github.com>
2023-02-17 12:37:24 +0100

    Servicio - Implementación de Vehículo

--- src/main/java/com/example/repositories/VehiculoRepository.java
+++ src/main/java/com/example/repositories/VehiculoRepository.java
@@ -1,7 +1,22 @@
 package com.example.repositories;
 
+import com.example.entities.Combustible;
+import com.example.entities.TipoVehiculo;
 import com.example.entities.Vehiculo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface VehiculoRepository extends JpaRepository<Vehiculo, Long> {
+    List<Vehiculo> findAllByModelo(String modelo);
+
+    List<Vehiculo> findAllByMarca(String marca);
+
+    List<Vehiculo> findAllByTipoVehículo(TipoVehiculo tipo);
+    List<Vehiculo> findAllByMatricula(String matricula);
+    List<Vehiculo> findAllByColor(String color);
+    List<Vehiculo> findAllByCombustible(Combustible combustible);
+    List<Vehiculo> findAllByYear(Integer year);
+
+    List<Vehiculo> findAllModeloAndMarca(String modelo, String marca);
 }
---
+++ src/main/java/com/example/service/VehiculoService.java
@@ -0,0 +1,19 @@
+package com.example.service;
+
+import com.example.entities.Address;
+import com.example.entities.Vehiculo;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface VehiculoService {
+    List<Vehiculo> findAll();
+    Optional<Vehiculo> findById(Long id);
+    List<Vehiculo> findAllByModelo(String modelo);
+    List<Vehiculo> findAllByMarca(String marca);
+    List<Vehiculo> findAllByModeloAndMarca(String modelo, String marca);
+    List<Vehiculo> findAllByMatricula(String matricula);
+    List<Vehiculo> findAllByColor(String color);
+    List<Vehiculo> findAllByYear(Integer year);
+    void deleteById(Long id);
+}
