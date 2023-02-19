5226d13dbc1cba4b28c74008dec3d570307bf34e
Robertron95 <robertron561@gmail.com>
2023-02-19 11:17:45 +0100

    Añadidos Overrides a métodos implementaciones de servicios Taller y Mecánico

--- src/main/java/com/example/service/MecanicoServiceImpl.java
+++ src/main/java/com/example/service/MecanicoServiceImpl.java
@@ -14,37 +14,44 @@ public class MecanicoServiceImpl implements MecanicoService {
 
     private final MecanicoRepository mecanicoRepository;
 
+
     @Override
     public List<Mecanico> findAll() {
 
         return mecanicoRepository.findAll();
     }
 
+    @Override
     public Optional<Mecanico> findById(Long id) {
 
         return mecanicoRepository.findById(id);
     }
 
+    @Override
     public List<Mecanico> findAllByFullname(String fullname) {
 
         return mecanicoRepository.findAllByFullname(fullname);
     }
 
+    @Override
     public List<Mecanico> findAllByTelefono(Integer telefono) {
 
         return mecanicoRepository.findAllByTelefono(telefono);
     }
 
+    @Override
     public List<Mecanico> findAllByFullnameAndTelefono(String fullname, Integer telefono) {
 
         return mecanicoRepository.findAllByFullnameAndTelefono(fullname, telefono);
     }
 
+    @Override
     public Mecanico save(Mecanico mecanico) {
 
         return mecanicoRepository.save(mecanico);
     }
 
+    @Override
     public void deleteById(Long id) {
 
         mecanicoRepository.deleteById(id);
--- src/main/java/com/example/service/TallerServiceImpl.java
+++ src/main/java/com/example/service/TallerServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.service;
 
 import com.example.entities.Taller;
+import com.example.repositories.MecanicoRepository;
 import com.example.repositories.TallerRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,32 +15,44 @@ public class TallerServiceImpl implements TallerService {
 
     private final TallerRepository tallerRepository;
 
+
     @Override
     public List<Taller> findAll() {
 
         return tallerRepository.findAll();
     }
 
+    @Override
     public Optional<Taller> findById(Long id) {
 
         return tallerRepository.findById(id);
     }
+
+    @Override
     public List<Taller> findAllByNombre(String nombre) {
 
         return  tallerRepository.findAllByNombre(nombre);
     }
+
+    @Override
     public List<Taller> findAllByTelefono(Integer telefono) {
 
         return tallerRepository.findAllByTelefono(telefono);
     }
+
+    @Override
     public List<Taller> findAllByNombreAndTelefono(String nombre, Integer telefono) {
 
         return tallerRepository.findAllByNombreAndTelefono(nombre, telefono);
     }
+
+    @Override
     public Taller save(Taller taller) {
 
         return tallerRepository.save(taller);
     }
+
+    @Override
     public void deleteById(Long id) {
 
         tallerRepository.deleteById(id);
