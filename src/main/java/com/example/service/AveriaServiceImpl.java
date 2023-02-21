ace2895fe63a96fce3e9d891211538231c5875f5
JoseIgnacioGJ <joigarja@gmail.com>
2023-02-21 10:27:45 +0100

    Creamos los métodos en el servicio para hacer las operaciones de Averia

---
+++ src/main/java/com/example/service/AveriaServiceImpl.java
@@ -0,0 +1,59 @@
+package com.example.service;
+
+import com.example.entities.Averia;
+import com.example.entities.Grado;
+import com.example.entities.TipoAveria;
+import com.example.repositories.AveriaRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+@Service
+public class AveriaServiceImpl implements AveriaService{
+
+    private final AveriaRepository averiaRepository;
+
+    @Override
+    public List<Averia> findAll() {
+        return averiaRepository.findAll();
+    }
+
+    @Override
+    public Optional<Averia> findById(Long id) {
+        return averiaRepository.findById(id);
+    }
+
+    @Override
+    public List<Averia> findAllByTipo(TipoAveria tipo) {
+        return averiaRepository.findAllByTipo(tipo);
+    }
+
+    @Override
+    public List<Averia> findAllByDescripcion(String descripcion) {
+        return averiaRepository.findAllByDescripcion(descripcion);
+    }
+
+    @Override
+    public List<Averia> findAllByFechaHora(LocalDateTime fecha_hora) {
+        return averiaRepository.findAllByFechaHora(fecha_hora);
+    }
+
+    @Override
+    public List<Averia> findAllByGrado(Grado grado) {
+        return averiaRepository.findAllByGrado(grado);
+    }
+
+    @Override
+    public Averia save(Averia averia) {
+        return averiaRepository.save(averia);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        averiaRepository.deleteById(id);
+    }
+}
