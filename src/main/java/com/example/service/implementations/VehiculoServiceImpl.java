b259c7f3c2c87543084ff27f5ad56ad51bfe3d88
“cristinabustamanterunde” <cristina.bustamanterunde@gmail.com>
2023-03-01 20:15:59 +0100

    Actualización Vehiculo

--- src/main/java/com/example/service/implementations/VehiculoServiceImpl.java
+++ src/main/java/com/example/service/implementations/VehiculoServiceImpl.java
@@ -13,47 +13,51 @@ import java.util.Optional;
 @AllArgsConstructor
 @Service
 public class VehiculoServiceImpl implements VehiculoService {
-
+    
     private final VehiculoRepository vehiculoRepository;
     
     @Override
-    public List<Vehiculo> findAll() { return vehiculoRepository.findAll(); }
-
+    public List<Vehiculo> findAll() {
+        return vehiculoRepository.findAll();
+    }
+    
     @Override
     public Optional<Vehiculo> findById(Long id) {
         return vehiculoRepository.findById(id);
     }
-
+    
     @Override
     public List<Vehiculo> findAllByModelo(String modelo) {
         return vehiculoRepository.findAllByModelo(modelo);
     }
-
+    
     @Override
     public List<Vehiculo> findAllByMarca(String marca) {
         return vehiculoRepository.findAllByMarca(marca);
     }
-
+    
     @Override
     public List<Vehiculo> findAllByModeloAndMarca(String modelo, String marca) {
-        return vehiculoRepository.findAllModeloAndMarca(modelo, marca);
+        return vehiculoRepository.findAllByModeloAndMarca(modelo, marca);
     }
-
+    
     @Override
     public List<Vehiculo> findAllByMatricula(String matricula) {
         return vehiculoRepository.findAllByMatricula(matricula);
     }
-
-    @Override
-    public List<Vehiculo> findAllByColor(String color) {
-        return vehiculoRepository.findAllByColor(color);
-    }
-
+    
     @Override
     public List<Vehiculo> findAllByYear(Integer year) {
         return vehiculoRepository.findAllByYear(year);
     }
-
+    
     @Override
-    public void deleteById(Long id) {vehiculoRepository.deleteById(id); }
+    public Vehiculo save(Vehiculo vehiculo) {
+        return vehiculoRepository.save(vehiculo);
+    }
+    
+    @Override
+    public void deleteById(Long id) {vehiculoRepository.deleteById(id);
+    
+    }
 }
